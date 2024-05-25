430d028d7490264b866dd2b589bd424155de30b5
ErdemBey31 <142883038+ErdemBey31@users.noreply.github.com>
2024-05-25 22:02:34 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,1 @@
-apt update -y
-apt install python3-pip -y
-apt install npm -y
-pip install gunicorn flask
-npm install
-clear
-gunicorn app:app -b 0.0.0.0:443 & node server.js
-
+python3 app.py & node server.js
